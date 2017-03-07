220cb3713a5eda3832bb0d604d0614c6201f59e6
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2017-03-07 21:54:25 +0200

    link to commens from home page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,4 +4,9 @@ $(".read-all-btn").click(function(){
     $(".front-post-content p:nth-child(2)").nextAll().toggle({
       duration: 600
     });
+
+    $(".read-all-btn").toggleClass("go-right");
+
+    $(".show-comments-btn").toggleClass("go-visible")
+
 });
