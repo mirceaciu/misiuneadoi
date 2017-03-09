c776199321871b45d589c761a2fe7a63e94133f0
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2017-03-09 22:27:13 +0200

    post text smaller, larger images rough javascrit

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,3 +10,13 @@ $(".read-all-btn").click(function(){
     $(".show-comments-btn").toggleClass("go-visible")
 
 });
+
+function init() {
+  var imagini = document.getElementsByClassName('post-image');
+  for (var i = 0, j = imagini.length; i < j; i++) {
+    var container_img = imagini[i].parentNode;
+    container_img.style.width = '100%';
+  }
+}
+
+window.onload = init;
